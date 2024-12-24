9304a93c9bb063bff1f01752c79273dccbbb1724
Sergey White <sergey_white86@mail.ru>
2024-12-24 14:32:01 +0300

    init commit

---
+++ src/test/java/org/sergey_white/dossier/DossierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergey_white.dossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
